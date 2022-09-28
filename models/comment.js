2add46520eeabac69e03fe1b803fe8ee90bffedc
EMILY MCEWEN <emmcewen@gmail.com>
2022-09-27 20:11:00 -0400

    adding models, added comment model

---
+++ models/comment.js
@@ -0,0 +1,46 @@
+const {Model, DataTypes} =require('sequelize');
+const sequelize = require('../config/connection');
+const Comment = require('./Post');
+
+Comment.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        content:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+
+        Post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
+
+module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -1,11 +1,26 @@
 const User = require ('./User');
 const Post = require ('./Post');
+const Comment = require ('./Comment');
 
 User.hasMany (Post,{
     foriegnKey:'user_id'
 });
 Post.belongsTo(User,{
     foriegnKey:'user_id'
+});
+
+Post.hasMany(Comment,{
+    foriegnKey:'post_id'
+});
+
+Comment.belongsTo(Post,{
+    foriegnKey: 'post_id',
+});
+Comment.belongsTo(User,{
+    foriegnKey:'user_id'
+});
+User.hasMany(Comment,{
+    foriegnKey: 'user_id'
 });
 
 module.exports = {User,Post};
